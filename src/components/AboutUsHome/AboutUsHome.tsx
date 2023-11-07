cb02a4400968dc6be3dae53c906f87626279aa67
Theo Pelegrina <71673834+theit0@users.noreply.github.com>
2023-11-07 00:24:58 -0300

    imagenes subidas para que carguen en el deploy

--- src/components/AboutUsHome/AboutUsHome.tsx
+++ src/components/AboutUsHome/AboutUsHome.tsx
@@ -9,7 +9,7 @@ const AboutUsHome = () => {
         <p>We are a team of passionate individuals who love to create amazing things. Our mission is to provide high-quality services and products that exceed our customers' expectations.</p>
         <section className='d-flex gap-4'>
           <Card style={{ width: '18rem',height:"fit-content"}}>
-            <Card.Img variant="top" src="src/assets/images/city-lights-in-darnkess.jpg" style={{height:"18rem",objectFit:"cover"}} />
+            <Card.Img variant="top" src="https://i.ibb.co/YkckLbH/city-lights-in-darnkess.jpg" style={{height:"18rem",objectFit:"cover"}} />
             <Card.Body>
               <Card.Title>Card Title</Card.Title>
               <Card.Text>
@@ -20,7 +20,7 @@ const AboutUsHome = () => {
             </Card.Body>
           </Card>
           <Card style={{ width: '18rem',height:"fit-content"}}>
-            <Card.Img variant="top" src="src/assets/images/lonely-lightbulb.jpg" style={{height:"18rem",objectFit:"cover"}}/>
+            <Card.Img variant="top" src="https://i.ibb.co/0KnqVwg/lonely-lightbulb.jpg" style={{height:"18rem",objectFit:"cover"}}/>
             <Card.Body>
               <Card.Title>Card Title</Card.Title>
               <Card.Text>
@@ -31,7 +31,7 @@ const AboutUsHome = () => {
             </Card.Body>
           </Card>
           <Card style={{ width: '18rem',height:"fit-content"}}>
-            <Card.Img variant="top" src="src/assets/images/ominous-portrait-of-orange-fish.jpg" style={{height:"18rem",objectFit:"cover"}}/>
+            <Card.Img variant="top" src="https://i.ibb.co/HtYxZKm/ominous-portrait-of-orange-fish.jpg" style={{height:"18rem",objectFit:"cover"}}/>
             <Card.Body>
               <Card.Title>Card Title</Card.Title>
               <Card.Text>
